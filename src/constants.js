742ab428c9a6b321ab6469232f07cce2f4bac44e
R Sarveshvarn <151063233+gdev-kingx@users.noreply.github.com>
2024-03-11 23:06:42 +0530

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,20 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
+    I've made a couple of games in that language. I also like Lua and Python. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/gdev-kingx" target="_blank">Github</a>!`,
+    "cs-degree": `This is my CS degree. I hanged it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+    "sofa-table": `That's my sofa. I like to relax here and watch Instagram. 
+    You can follow me on <a href="http://www.instagram.com/gdev._king.x" target="_blank">Instagram</a> by clicking here.`, 
+    tv: `That's my TV. I've been watching Coding Tutorials a lot recently like:
+    <a href="https://www.youtube.com/@TechWithTim" target="_blank">TechWithTim</a>, <a href="https://www.youtube.com/@Frankslaboratory" target="_blank">FranksLaboratory</a>,
+    <a href="https://www.youtube.com/@programmingwithnick" target="_blank">ProgrammingWithNick</a> (sometimes) and <a href="https://www.youtube.com/@ClearCode" target="_blank">ClearCode</a>!`,
+    bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+    resume: `This is my desk and on it is my resume. <a href="https://github.com/gdev-kingx/Resume/blob/main/Resume-SARVESHVARN-RAMPRASAD.pdf" target="_blank">Check it out?</a>
+    Contact me at sarveshvarnr@gmail.com if you have any interesting job opportunities!`,
+    projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+    Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+    library: `There are a lot of programming books on my shelves. There is even one in German (I also speak German btw).
+    I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+}
